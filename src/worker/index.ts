a0661013e629f902a3ce1cf12cd13885a33e9fea
N7CmiYazaki <miyazakitoma@n7create.com>
2026-05-23 21:36:24 +0900

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,4 +1,10 @@
 import { Hono } from "hono";
+
+interface Env {
+  DB: D1Database; // wrangler.jsonのd1_databasesで指定したbinding名
+  KV?: KVNamespace; // 必要に応じて追加（使っていなければ消してもOKです）
+}
+
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
