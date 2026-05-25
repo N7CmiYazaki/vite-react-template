52b72a4c3c42b2a337170984c826eec3b1016b67
N7CmiYazaki <miyazakitoma@n7create.com>
2026-05-25 17:26:23 +0900

    これといって何も書くことない

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './index' // あなたが書いた index.tsx を読み込む
+
+// "root" という名前の場所に、App（ハンバーガーメニュー）を映し出す
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
